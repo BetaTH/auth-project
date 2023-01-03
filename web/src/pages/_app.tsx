be099c4adf19bacf0da63d6b777bebb061322111
Thielson Almendra <thielson12@gmail.com>
2023-01-03 11:26:18 -0300

    feat: add progressbar on change page

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import "../styles/global.css";
 import type { AppProps } from "next/app";
 import { AuthProvider, User } from "../contexts/AuthContext";
+import NextNProgress from "nextjs-progressbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider
       userData={pageProps?.userData ? (pageProps?.userData as User) : null}
     >
+      <NextNProgress color="#432eb5" height={3} />
       <Component {...pageProps} />
     </AuthProvider>
   );
