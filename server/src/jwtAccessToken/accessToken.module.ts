491f4461996f1a482bf73f97bcaa5e246ec5afbc
Thielson <thielson12@gmail.com>
2023-01-31 10:59:46 -0300

    refactor: remove user modules from jwts modules

--- server/src/jwtAccessToken/accessToken.module.ts
+++ server/src/jwtAccessToken/accessToken.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { UserModule } from 'src/users/user.module';
 import { AccessTokenService } from './accessToken.service';
 
 @Module({
   imports: [
-    UserModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '20s' },
--- server/src/jwtRefreshToken/refreshToken.module.ts
+++ server/src/jwtRefreshToken/refreshToken.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { UserModule } from 'src/users/user.module';
 import { RefreshTokenService } from './refreshToken.service';
 
 @Module({
   imports: [
-    UserModule,
     JwtModule.register({
       secret: process.env.JWT_REFRESH_SECRET,
       signOptions: { expiresIn: '30d' },
